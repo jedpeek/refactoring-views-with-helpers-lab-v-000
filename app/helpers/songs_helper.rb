5aeaeef7bcb8b58ebfe075ac1baa739c32477de9
Jed Peek <jedpeek@yahoo.com>
2018-08-24 01:11:18 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -8,4 +8,12 @@ module SongsHelper
     end
   end
 
+  def song_link(song)
+    if song.artist
+      link_to song.title, song_path(song)
+    else
+      link_to "Add Artist", edit_song_path(song)
+    end
+  end
+
 end
